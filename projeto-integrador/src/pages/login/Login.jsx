cb29c1fa5bfd9f25f12b1c6f1dad5693c882e171
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-28 14:40:47 -0300

    atualiza layout e estilos da página de login

--- projeto-integrador/src/pages/login/Login.jsx
+++ projeto-integrador/src/pages/login/Login.jsx
@@ -1,41 +1,39 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import styles from './Login.module.css'
+import styles from "./Login.module.css";
 
 const Login = () => {
   const [role, setRole] = useState("");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function handleLogin() {
-    navigate('/Inicio')
+    navigate("/Inicio");
   }
 
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h2>Login</h2>
-
+        <img src="#" alt="Logo" />
+        <h3>Nome do sistema</h3>
+        <p>Faça login</p>
         <select
           className={styles.select}
           value={role}
           onChange={(e) => setRole(e.target.value)}
         >
-          <option value="">Selecione o tipo de usuário</option>
-          <option value="perito">Perito</option>
-          <option value="admin">Admin</option>
-          <option value="assistente">Assistente</option>
+          <option value="">Selecione o cargo</option>
+          <option value="perito">Admin</option>
+          <option value="admin">Assistente</option>
+          <option value="assistente">Perito</option>
         </select>
-
         <input type="email" placeholder="Email" className={styles.input} />
         <input type="password" placeholder="Senha" className={styles.input} />
-
-        <button 
-          className={styles.button}
-          onClick={handleLogin}
-        >
-            Entrar
+        <button className={styles.button} onClick={handleLogin}>
+          Entrar
         </button>
-        <Link to="/nova-senha" className={styles.link}>Esqueceu a senha?</Link>
+        <Link to="/nova-senha" className={styles.link}>
+          Esqueceu a senha?
+        </Link>
       </div>
     </div>
   );
