5bca759e16610ad24a77caecf255556d08b01124
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-29 19:09:41 -0300

    adiciona novos campos de endereço ao formulário de registro de usuário e ajusta a margem do componente

--- projeto-integrador/src/pages/user_registration_page/UserRegistrationPage.jsx
+++ projeto-integrador/src/pages/user_registration_page/UserRegistrationPage.jsx
@@ -9,7 +9,7 @@ const UserRegistrationPage = () => {
             <div className={styles.nav_container}>
                 <NavAdmin />
                 <div className={styles.user_registration}>
-                    <h3>Insira as informações de perfil</h3>
+                    <h3>Insira as informações do usuário</h3>
                     <label htmlFor="name">Nome:</label>
                     <input type="text" id="name" placeholder="Insira o nome completo" className={styles.imput}/>
                     <label htmlFor="email">Email:</label>
@@ -29,6 +29,20 @@ const UserRegistrationPage = () => {
                     <input type="password" id="senha" placeholder="Insira a senha" className={styles.imput}/>
                     <label htmlFor="confirmarSenha">Confirme a senha:</label>
                     <input type="password" id="confirmarSenha" placeholder="Confirme a senha" className={styles.imput}/>
+                    <label htmlFor="rua">Rua:</label>
+                    <input type="text" id="rua" placeholder="Insira a rua" className={styles.imput} />
+                    <label htmlFor="numero">Número:</label>
+                    <input type="text" id="numero" placeholder="Insira o número" className={styles.imput}/>
+                    <label htmlFor="bairro">Bairro:</label>
+                    <input type="text" id="bairro" placeholder="Insira o bairro" className={styles.imput}/>
+                    <label htmlFor="cidade">Cidade:</label>
+                    <input type="text" id="cidade" placeholder="Insira a cidade" className={styles.imput}/>
+                    <label htmlFor="estado">Estado:</label>
+                    <input type="text" id="estado" placeholder="Insira o estado" className={styles.imput}/>
+                    <label htmlFor="cep">CEP:</label>
+                    <input type="text" id="cep" placeholder="Insira o CEP" className={styles.imput}/>
+                    <label htmlFor="complemento">Complemento:</label>
+                    <input type="text" id="complemento" placeholder="Insira o complemento" className={styles.imput}/>
                 </div>
             </div>
         </div>
