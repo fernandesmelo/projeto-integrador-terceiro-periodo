2fef22af6d30f8586f27b3fbbd7265ddd31341d7
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-29 14:05:34 -0300

    refatoração: melhora a legibilidade do componente Header e remove código comentado

--- projeto-integrador/src/components/header/Header.jsx
+++ projeto-integrador/src/components/header/Header.jsx
@@ -1,29 +1,24 @@
-import { useState } from 'react';
-import styles from './Header.module.css'
+import { useState } from "react";
+import styles from "./Header.module.css";
 import { FaBell } from "react-icons/fa";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 
 const Header = () => {
+  const [ativarMenu, setAtivarMenu] = useState(false);
 
-    const [ativarMenu, setAtivarMenu] = useState(false)
+  return (
+    <div className={styles.content}>
+      <p>Nome do sistema</p>
+      <div className={styles.info}>
+        <FaBell />
+        <div>
+          <p className={styles.role}>Cargo</p>
+          <p className={styles.name}>Nome do usuário</p>
+        </div>
+        <MdOutlineKeyboardArrowDown size={30} onClick={() => setAtivarMenu(!ativarMenu)} />
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.content}>
-            <p>OdontoLegal</p>
-                <div className={styles.info}>
-                    <FaBell />
-                    {/* <img src="" alt="" className={styles.photo} /> */}
-                    <div>
-                        <p className={styles.role}>Cargo -</p>
-                        <p className={styles.name}>Nome Sobrenome</p>
-                    </div>
-                    <MdOutlineKeyboardArrowDown 
-                    size={30}
-                    onClick={() => setAtivarMenu(!ativarMenu)}
-                    />
-                </div>
-            </div>
-    )
-}
-
-export default Header
+export default Header;
