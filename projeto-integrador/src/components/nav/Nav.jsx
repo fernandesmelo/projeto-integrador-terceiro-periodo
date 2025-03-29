3a76295b356684ebbaba17c563445a4664274e1a
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-03-29 13:44:14 -0300

    refatoração: simplifica a navegação removendo ícones e mantendo apenas os itens essenciais

--- projeto-integrador/src/components/nav/Nav.jsx
+++ projeto-integrador/src/components/nav/Nav.jsx
@@ -1,9 +1,4 @@
 import styles from './Nav.module.css';
-import { CgScreen } from "react-icons/cg";
-import { MdAdminPanelSettings } from "react-icons/md";
-import { TbAlertHexagonFilled } from "react-icons/tb";
-import { RiToothFill, RiFileShield2Fill } from "react-icons/ri";
-import { PiFilesFill } from "react-icons/pi";
 import { useNavigate } from 'react-router-dom';
 
 const Nav = () => {
@@ -16,24 +11,8 @@ const Nav = () => {
     return (
         <div>
             <div className={styles.nav}>
-                <p className={styles.item} tabIndex={0} role="button" onClick={() => navigate('/')}>
-                    <CgScreen size={31} strokeWidth={1} style={{ color: "var(--icons)" }} /> Início
-                </p>
-                <p className={styles.item} tabIndex={0} role="button">
-                    <RiFileShield2Fill size={32} style={{ color: "var(--icons)" }} /> Casos Periciais
-                </p>
-                <p className={styles.item} tabIndex={0} role="button">
-                    <TbAlertHexagonFilled size={32} style={{ color: "var(--icons)" }} /> Evidências
-                </p>
-                <p className={styles.item} tabIndex={0} role="button">
-                    <PiFilesFill size={32} style={{ color: "var(--icons)" }} /> Laudos
-                </p>
-                <p className={styles.item} tabIndex={0} role="button">
-                    <RiToothFill size={32} style={{ color: "var(--icons)" }} /> Banco Odonto-Legal
-                </p>
-                <p className={styles.item} tabIndex={0} role="button" onClick={handleAdmin}>
-                    <MdAdminPanelSettings size={32} style={{ color: "var(--icons)" }} /> Admin
-                </p>
+                <p className={styles.item} tabIndex={0} role="button" onClick={() => navigate('/')}>Início</p>
+                <p className={styles.item} tabIndex={0} role="button" onClick={handleAdmin}>Admin</p>
             </div>
         </div>
     );
