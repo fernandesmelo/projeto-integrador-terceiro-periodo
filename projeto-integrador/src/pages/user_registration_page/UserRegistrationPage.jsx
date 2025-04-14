feefbd645fd61666fc15afabcac92329011b349f
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-04-14 15:52:38 -0300

    adiciona contêineres div em torno dos campos de entrada para melhor estruturação na página de registro de usuário

--- projeto-integrador/src/pages/user_registration_page/UserRegistrationPage.jsx
+++ projeto-integrador/src/pages/user_registration_page/UserRegistrationPage.jsx
@@ -82,141 +82,171 @@ const UserRegistrationPage = () => {
                     <h3>Insira as informações do usuário</h3>
                     <form onSubmit={handleSubmit}>
                         <label htmlFor="name">Nome:</label>
-                        <input
-                            type="text"
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="Insira o nome completo"
-                            className={styles.input}
-                        />
-                         <label htmlFor="lastName">Sobrenome:</label>
-                        <input
-                            type="text"
-                            placeholder="Sobrenome"
-                            value={lastName}
-                            onChange={(e) => setLastName(e.target.value)}
-                            className={styles.input}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                                placeholder="Insira o nome completo"
+                                className={styles.input}
+                            />
+                        </div>
+                        <label htmlFor="lastName">Sobrenome:</label>
+                        <div>
+                            <input
+                                type="text"
+                                placeholder="Sobrenome"
+                                value={lastName}
+                                onChange={(e) => setLastName(e.target.value)}
+                                className={styles.input}
+                            />
+                        </div>
                         <label htmlFor="email">Email:</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            placeholder="Insira o email"
-                            className={styles.input}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="email"
+                                id="email"
+                                value={email}
+                                placeholder="Insira o email"
+                                className={styles.input}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
                         <label htmlFor="dataNascimento">Data de Nascimento:</label>
-                        <input
-                            type="date"
-                            id="dataNascimento"
-                            className={styles.input}
-                            value={dateOfBirth}
-                            onChange={(e) => setDateOfBirth(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="date"
+                                id="dataNascimento"
+                                className={styles.input}
+                                value={dateOfBirth}
+                                onChange={(e) => setDateOfBirth(e.target.value)}
+                            />
+                        </div>
                         <label htmlFor="cpf">CPF:</label>
-                        <input
-                            type="text"
-                            id="cpf"
-                            placeholder="Insira o CPF"
-                            className={styles.input}
-                            value={cpf}
-                            onChange={(e) => setCpf(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="cpf"
+                                placeholder="Insira o CPF"
+                                className={styles.input}
+                                value={cpf}
+                                onChange={(e) => setCpf(e.target.value)}
+                            />
+                        </div>
                         <label htmlFor="cargo">Cargo:</label>
-                        <select
-                            id="cargo"
-                            value={role}
-                            onChange={(e) => setRole(e.target.value)}
-                            className={styles.input}
-                        >
-                            <option value="">Selecione o cargo</option>
-                            <option value="ADMIN">Admin</option>
-                            <option value="ASSISTENTE">Assistente</option>
-                            <option value="PERITO">Perito</option>
-                        </select>
+                        <div>
+                            <select
+                                id="cargo"
+                                value={role}
+                                onChange={(e) => setRole(e.target.value)}
+                                className={styles.input}
+                            >
+                                <option value="">Selecione o cargo</option>
+                                <option value="ADMIN">Admin</option>
+                                <option value="ASSISTENTE">Assistente</option>
+                                <option value="PERITO">Perito</option>
+                            </select>
+                        </div>
                         <label htmlFor="senha">Senha:</label>
-                        <input
-                            type="password"
-                            id="senha"
-                            placeholder="Insira a senha"
-                            className={styles.input}
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="password"
+                                id="senha"
+                                placeholder="Insira a senha"
+                                className={styles.input}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
                         <label htmlFor="confirmarSenha">Confirme a senha:</label>
-                        <input
-                            type="password"
-                            id="confirmarSenha"
-                            value={confirmPassword}
-                            onChange={(e) => setConfirmPassword(e.target.value)}
-                            placeholder="Confirme a senha"
-                            className={styles.input}
-                        />
+                        <div>
+                            <input
+                                type="password"
+                                id="confirmarSenha"
+                                value={confirmPassword}
+                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                placeholder="Confirme a senha"
+                                className={styles.input}
+                            />
+                        </div>
                         <label htmlFor="rua">Rua:</label>
-                        <input
-                            type="text"
-                            id="rua"
-                            placeholder="Insira a rua"
-                            value={road}
-                            onChange={(e) => setRoad(e.target.value)}
-                            className={styles.input}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="rua"
+                                placeholder="Insira a rua"
+                                value={road}
+                                onChange={(e) => setRoad(e.target.value)}
+                                className={styles.input}
+                            />
+                        </div>
                         <label htmlFor="numero">Número:</label>
-                        <input
-                            type="number"
-                            id="numero"
-                            value={houseNumber}
-                            onChange={(e) => setHouseNumber(Number(e.target.value))}
-                            placeholder="Insira o número"
-                            className={styles.input}
-                        />
+                        <div>
+                            <input
+                                type="number"
+                                id="numero"
+                                value={houseNumber}
+                                onChange={(e) => setHouseNumber(Number(e.target.value))}
+                                placeholder="Insira o número"
+                                className={styles.input}
+                            />
+                        </div>
                         <label htmlFor="bairro">Bairro:</label>
-                        <input
-                            type="text"
-                            id="bairro"
-                            placeholder="Insira o bairro"
-                            className={styles.input}
-                            value={neighborhood}
-                            onChange={(e) => setNeighborhood(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="bairro"
+                                placeholder="Insira o bairro"
+                                className={styles.input}
+                                value={neighborhood}
+                                onChange={(e) => setNeighborhood(e.target.value)}
+                            />
+                        </div>
                         <label htmlFor="cidade">Cidade:</label>
-                        <input
-                            type="text"
-                            id="cidade"
-                            placeholder="Insira a cidade"
-                            className={styles.input}
-                            value={city}
-                            onChange={(e) => setCity(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="cidade"
+                                placeholder="Insira a cidade"
+                                className={styles.input}
+                                value={city}
+                                onChange={(e) => setCity(e.target.value)}
+                            />
+                        </div>
                         <label htmlFor="estado">Estado:</label>
-                        <input
-                            type="text"
-                            id="estado"
-                            placeholder="Insira o estado"
-                            value={state}
-                            onChange={(e) => setState(e.target.value)}
-                            className={styles.input}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="estado"
+                                placeholder="Insira o estado"
+                                value={state}
+                                onChange={(e) => setState(e.target.value)}
+                                className={styles.input}
+                            />
+                        </div>
                         <label htmlFor="cep">CEP:</label>
-                        <input
-                            type="text"
-                            id="cep"
-                            placeholder="Insira o CEP"
-                            className={styles.input}
-                            value={cep}
-                            onChange={(e) => setCep(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="cep"
+                                placeholder="Insira o CEP"
+                                className={styles.input}
+                                value={cep}
+                                onChange={(e) => setCep(e.target.value)}
+                            /> 
+                        </div>
                         <label htmlFor="complemento">Complemento:</label>
-                        <input
-                            type="text"
-                            id="complemento"
-                            placeholder="Insira o complemento"
-                            className={styles.input}
-                            value={complement}
-                            onChange={(e) => setComplement(e.target.value)}
-                        />
+                        <div>
+                            <input
+                                type="text"
+                                id="complemento"
+                                placeholder="Insira o complemento"
+                                className={styles.input}
+                                value={complement}
+                                onChange={(e) => setComplement(e.target.value)}
+                            />
+                        </div>
                         <button type="submit" className={styles.button}>Cadastrar</button>
                     </form>
 
