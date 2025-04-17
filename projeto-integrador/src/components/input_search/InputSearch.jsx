1a93cc246d815da4919f68ab4d43cf802a153c22
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-04-17 19:22:22 -0300

    estilo: refatora componente InputSearch para melhorar a legibilidade e a consistência do código

--- projeto-integrador/src/components/input_search/InputSearch.jsx
+++ projeto-integrador/src/components/input_search/InputSearch.jsx
@@ -1,26 +1,24 @@
-import styles from './InputSearch.module.css'
-import { FaSearch } from "react-icons/fa";
-import clsx from 'clsx';
+import styles from "./InputSearch.module.css";
+import clsx from "clsx";
 
+const InputSearch = ({ placeholder, variant, value, onChange, onKeyDown }) => {
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+      }}
+      className={styles.searchContainer}
+    >
+      <input
+        className={clsx(styles.input, styles[variant])}
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+    </form>
+  );
+};
 
-const InputSearch = ({placeholder, variant, value, onChange, onKeyDown}) => {
-    return (
-        <form onSubmit={(e) => {
-            e.preventDefault()
-        }} className={styles.searchContainer}>
-            <input
-                className={clsx(styles.input, styles[variant])}
-                type="text"
-                placeholder={placeholder}
-                value={value}
-                onChange={onChange}
-                onKeyDown={onKeyDown}
-            />
-        </form>
-
-
-
-    )
-}
-
-export default InputSearch
+export default InputSearch;
