e05098c7c3e7ae87fb6badc81a4caacbe52babde
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-04-17 20:32:49 -0300

    estilo: ajusta a formatação do componente HomePage para melhorar a legibilidade e a consistência

--- projeto-integrador/src/pages/home_page/HomePage.jsx
+++ projeto-integrador/src/pages/home_page/HomePage.jsx
@@ -2,16 +2,15 @@ import Header from "../../components/header/Header";
 import Nav from "../../components/nav/Nav";
 import styles from "./HomePage.module.css";
 
-
 const Inicio = () => {
-    return (
-        <div className={styles.container}>
-            <Header />
-            <div>
-                <Nav />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+      <Header />
+      <div>
+        <Nav />
+      </div>
+    </div>
+  );
+};
 
 export default Inicio;
