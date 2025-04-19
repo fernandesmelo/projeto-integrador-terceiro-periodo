fc3b4ef7b9bc0ddac25a47be844edb54d0e22796
Matheus Bezerra <matheusbzrr3@gmail.com>
2025-04-19 16:33:20 -0300

    add: dashboard

--- projeto-integrador/src/pages/home_page/HomePage.jsx
+++ projeto-integrador/src/pages/home_page/HomePage.jsx
@@ -4,6 +4,7 @@ import styles from "./HomePage.module.css";
 import MapaD3Geojson from "../../components/map/MapComponent";
 import CasesBarChart from "../../components/bar_charts/CasesBarCharts";
 import PieCharts from "../../components/pie_chart/PieCharts";
+import VictimsStatusDoughnut from "../../components/doughnut_chart/DoughnutChart";
 
 const Inicio = () => {
   return (
@@ -17,16 +18,18 @@ const Inicio = () => {
               <div className={`${styles.dataCard} ${styles.revenueCard}`}>
                 <MapaD3Geojson />
               </div>
-              <div
-                className={`${styles.dataCard} ${styles.customerCard}`}
-              >
-                <CasesBarChart/>
+              <div className={`${styles.dataCard} ${styles.customerCard}`}>
+                <CasesBarChart />
               </div>
-              
             </div>
-            <div className={`${styles.dataCard} ${styles.categoryCard}`}>
-                <PieCharts/>
+            <div className={styles.row}>
+              <div className={`${styles.dataCard} ${styles.categoryCard}`}>
+                <PieCharts />
               </div>
+              <div className={`${styles.dataCard} ${styles.categoryCard}`}>
+                <VictimsStatusDoughnut />
+              </div>
+            </div>
           </div>
         </div>
       </div>
