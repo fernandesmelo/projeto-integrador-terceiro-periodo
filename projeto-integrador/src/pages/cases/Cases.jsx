fce2253eb7008a803efa0850b031a2c615f76476
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-04-20 11:40:56 -0300

    refatoração: renomeia a variante do botão de 'button-cases' para 'button-table' para consistência

--- projeto-integrador/src/pages/cases/Cases.jsx
+++ projeto-integrador/src/pages/cases/Cases.jsx
@@ -161,7 +161,7 @@ const Cases = () => {
           <div className={styles.buttons}>
             <Button
               type="button"
-              variant="button-cases"
+              variant="button-table"
               onClick={() => navigate("/casos/cadastrarVitima")}
               disabled={false}
             >
