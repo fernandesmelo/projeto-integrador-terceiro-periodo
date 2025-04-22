26e0862f6f776b7eebf7822f37d33cf38186bd3d
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-04-22 16:44:43 -0300

    refatoração: ajusta a formatação e a estrutura do componente Nav2

--- projeto-integrador/src/components/nav2/Nav2.jsx
+++ projeto-integrador/src/components/nav2/Nav2.jsx
@@ -1,37 +1,32 @@
-import Swal from 'sweetalert2'
-import styles from './Nav2.module.css'
+import Swal from "sweetalert2";
+import styles from "./Nav2.module.css";
 
-const Nav2 = ({content, onClick}) => {
+const Nav2 = ({ content, onClick }) => {
+  const handleClick = () => {
+    Swal.fire({
+      title: "Tem certeza?",
+      text: "Você realmente deseja voltar?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#1E88E5",
+      cancelButtonColor: "#EB5757",
+      confirmButtonText: "Sim, voltar",
+      cancelButtonText: "Cancelar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onClick();
+      }
+    });
+  };
 
-    const handleClick = () => {
-      Swal.fire({
-        title: "Tem certeza?",
-        text: "Você realmente deseja voltar?",
-        icon: "warning",
-        showCancelButton : true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        confirmButtonText: "Sim, voltar",
-        cancelButtonText: "Cancelar"
-      }).then((result) => {
-        if (result.isConfirmed) {
-          onClick()
-        }
-      })
-    }
+  return (
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <a onClick={handleClick}>{content}</a>
+      </nav>
+      <hr />
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.navContainer}>
-            <nav className={styles.nav}>
-                <a onClick={handleClick}>{content}</a>
-            </nav>
-            <hr />
-        </div>
-
-       
-
-    )
-}
-
-export default Nav2
-
+export default Nav2;
